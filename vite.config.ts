561581b47830d6daddae3f22dcf0353c1b2c4840
anthony.m <anthony.m@skill-mine.com>
2026-02-17 17:47:45 +0530

    feat: Configure Vite to force `application/javascript` content type for `.tsx` files.

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,19 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    {
+      name: 'force-js-mime-type-tsx',
+      configureServer(server) {
+        server.middlewares.use((req, res, next) => {
+          if (req.url?.endsWith('.tsx')) {
+            res.setHeader('Content-Type', 'application/javascript')
+          }
+          next()
+        })
+      }
+    },
+    react(),
+    tailwindcss()
+  ],
 })
