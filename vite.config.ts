ccd9029ee8fd253d8510e19e550831a44e9723ba
anthony.m <anthony.m@skill-mine.com>
2026-02-17 17:58:15 +0530

    refactor: Remove custom TSX MIME type plugin and add base path configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,10 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    {
-      name: 'force-js-mime-type-tsx',
-      enforce: 'pre',
-      configureServer(server) {
-        server.middlewares.use((req, res, next) => {
-          if (req.url && /\.tsx($|\?)/.test(req.url)) {
-            console.log(`[Middleware] Forcing JS MIME type for: ${req.url}`)
-            res.setHeader('Content-Type', 'application/javascript')
-          }
-          next()
-        })
-      }
-    },
-    react(),
-    tailwindcss()
-  ],
+  plugins: [react(), tailwindcss()],
+  base: './',
 })
