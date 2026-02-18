655e23647c5a3685aeff37ca5f4f743372f4e93c
anthony.m <anthony.m@skill-mine.com>
2026-02-18 08:09:07 +0530

    add github repo

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -59,7 +59,7 @@ export const portfolioData = {
         phone: "+91 8220620376",
         mapUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31487.625347318854!2d77.7785591!3d9.4542283!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b06cf3096b7617b%3A0x633d45ca46285430!2sSivakasi%2C%20Tamil%20Nadu!5e0!3m2!1sen!2sin!4v1716301211111!5m2!1sen!2sin",
         socials: [
-            { label: "GitHub", href: "https://github.com/Anthony-Arul-Selvam", icon: Github },
+            { label: "GitHub", href: "https://github.com/MASAnthony?tab=repositories", icon: Github },
             { label: "LinkedIn", href: "https://linkedin.com/in/anthony-arul-selvam", icon: Linkedin },
             { label: "Email", href: "mailto:masanthony3a3@gmail.com", icon: Mail },
         ]
